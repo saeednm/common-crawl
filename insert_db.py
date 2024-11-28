77a106dec5d421b23a0ee4969960bbd44272468a
saeed <saeed.najdmohtasham@gmail.com>
2024-11-28 13:27:26 +0100

    read input and insert db

---
+++ insert_db.py
@@ -0,0 +1,61 @@
+import psycopg2
+import psycopg2.extras
+import io
+import string 
+import os
+
+txt_file_directory = "links_txt"
+
+def insert_links_to_db(links, db_config):
+    # Establish a database connection
+    conn = psycopg2.connect(**db_config)
+    cursor = conn.cursor()
+
+    # Convert data to a CSV-like string (without headers)
+    buffer = io.StringIO()
+    for row in links:
+        buffer.write('\t'.join([str(field) for field in row]) + '\n')
+
+    # Move the cursor back to the beginning of the StringIO buffer
+    buffer.seek(0)
+
+    # Perform the bulk insert with copy_from
+    cursor.copy_from(buffer, 'external_links', columns=('link',))
+
+    
+
+    # insert_query = 'INSERT INTO external_links (link) VALUES (%s)'
+    # psycopg2.extras.execute_values (cursor, insert_query, links, template=None, page_size=10)
+
+
+
+    # Commit the transaction and close the connection
+    conn.commit()
+    cursor.close()
+    conn.close()
+
+# Database configuration
+db_config = {
+    "dbname": "postgres",
+    "user": "postgres",
+    "password": "example",
+    "host": "localhost",
+    "port": 5432
+}
+
+
+
+
+def read_and_insert_links_to_db():
+    links = []
+
+    with os.scandir(txt_file_directory) as iter:
+            for entry in iter:
+                if entry.name.endswith(".warc.gz.txt") and entry.is_file():
+                    print(entry.name, entry.path)
+                    with open(entry.path, 'r') as file:
+                        for line in file:
+                            links.append((line),)
+
+    # Insert links into the database
+    insert_links_to_db(links, db_config)
---
+++ open_file.py
@@ -0,0 +1,70 @@
+from warcio.archiveiterator import ArchiveIterator
+from bs4 import BeautifulSoup
+from urllib.parse import urlparse
+import os
+import validators
+
+warc_file_directory = "commoncrawl_segments"
+txt_file_directory = "links_txt"
+
+def extract_external_links(warc_file):
+    external_links = {}  # Store links in a dictionary 
+
+    # Open the WARC file
+    with open(warc_file, 'rb') as stream:
+        for record in ArchiveIterator(stream):
+            if record.rec_type == 'response':  # Look for HTTP response records
+                url = record.rec_headers.get_header('WARC-Target-URI')
+                content_type = record.http_headers.get_header('Content-Type')
+
+                # Process HTML content only
+                if content_type and 'text/html' in content_type:
+                    payload = record.content_stream().read()
+                    try:
+                        soup = BeautifulSoup(payload, 'html.parser')
+
+                        # Extract all anchor tags with href
+                        for link in soup.find_all('a', href=True):
+                            href = link['href'].strip()
+                            parsed_href = urlparse(href)
+
+                            # Check if the link is external
+                            if parsed_href.scheme in ('http', 'https') and urlparse(url).netloc != parsed_href.netloc:
+                                if url not in external_links:
+                                    external_links[url] = []
+                                external_links[url].append(href)
+                    except:
+                        pass
+
+    return external_links
+
+
+
+
+# Save the external links to a file
+def save_links_to_file(links, filename):
+    file_complete_name = os.path.join(txt_file_directory, filename)
+    os.makedirs(os.path.dirname(file_complete_name), exist_ok=True)
+    with open(file_complete_name, "w") as file:
+        for page, external_links in links.items():
+            for link in external_links:
+                if validators.url(link, private=False): # filter malformatted urls
+                    file.write(f"{link}\n")  # Write one link per line
+
+
+# Example usage
+#warc_file = "commoncrawl_segments/CC-MAIN-20241101184224-20241101214224-00000.warc.gz"
+#links = extract_external_links(warc_file)
+#save_links_to_file(links, "links.txt")
+
+def extract_and_save_external_links():
+    with os.scandir(warc_file_directory) as iter:
+        for entry in iter:
+            if entry.name.endswith(".warc.gz") and entry.is_file():
+                print(entry.name, entry.path)
+                links = extract_external_links( entry.path)
+                save_links_to_file(links, entry.name + ".txt")
+
+    print("done!")
+
+extract_and_save_external_links()
